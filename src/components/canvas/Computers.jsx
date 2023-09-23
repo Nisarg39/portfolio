152549d30fa9601c8e6995f68dcc263cde099992
Nisarg Shah <shah.nisarg39@gmail.com>
2023-09-24 03:50:59 +0530

    added alpha property to computer canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -53,6 +53,7 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       camera={{position: [20, 3, 5], fov: 25}}
+      alpha= 'true'
       gl={{preserveDrawingBuffer: true}}
     >
       <Suspense fallback={<CanvasLoader />}>
